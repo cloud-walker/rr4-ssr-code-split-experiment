fa3b2cbd7d5f0b83e8b37655268d0c8fc0233137
Luca Barone <baro.luc@gmail.com>
2017-04-21 20:28:19 +0200

    :construction: add client bundle and bare webpack client/server config

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const {resolve} = require('path')
+const nodeExternals = require('webpack-node-externals')
+
+const sourcePath = resolve(__dirname, 'source')
+const buildPath = resolve(__dirname, 'build')
+
+module.exports = [
+  // Server config
+  {
+    entry: resolve(sourcePath, 'server'),
+    output: {
+      filename: 'server.js',
+    },
+    externals: [nodeExternals()],
+  },
+
+  // Client config
+  {
+    entry: resolve(sourcePath, 'client'),
+    output: {
+      path: buildPath,
+      filename: '[name].js',
+    },
+  },
+]
