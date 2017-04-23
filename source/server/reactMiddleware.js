d9df0b9c64b143ede5525df361778c6157496275
Luca Barone <baro.luc@gmail.com>
2017-04-23 16:08:29 +0200

    :sparkles: introduce react-router 4 on the server side

--- source/server/reactMiddleware.js
+++ source/server/reactMiddleware.js
@@ -1,12 +1,18 @@
 const React = require('react')
 const {renderToString} = require('react-dom/server')
+const {StaticRouter} = require('react-router')
 
 const createLayout = require('./createLayout')
 const App = require('shared/components/App')
 
-const content = renderToString(<App />)
-const html = createLayout({content})
-
 module.exports = ctx => {
+  const routerContext = {}
+  const content = renderToString(
+    <StaticRouter location={ctx.request.url} context={routerContext}>
+      <App />
+    </StaticRouter>
+  )
+  const html = createLayout({content})
+
   ctx.body = html
 }
--- source/shared/components/App/App.js
+++ source/shared/components/App/App.js
@@ -1,5 +1,20 @@
 const React = require('react')
+const {Route, Link} = require('react-router-dom')
 
-const Component = () => <div>React is ready.</div>
+const Component = () =>
+  <div>
+    <h2>App</h2>
+    <nav>
+      <Link to="/">home</Link>
+      {' '}
+      <Link to="/foo">foo</Link>
+      {' '}
+      <Link to="/bar">bar</Link>
+    </nav>
+
+    <Route exact path="/" render={() => <div>home</div>} />
+    <Route path="/foo" render={() => <div>foo</div>} />
+    <Route path="/bar" render={() => <div>bar</div>} />
+  </div>
 
 module.exports = Component
