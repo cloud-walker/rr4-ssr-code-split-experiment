788bbb375cb05c44eb1a72e2498d5b5486856c33
Luca Barone <baro.luc@gmail.com>
2017-04-26 11:44:22 +0200

    add redux integration

--- source/client/index.js
+++ source/client/index.js
@@ -3,16 +3,22 @@ import {render} from 'react-dom'
 import {BrowserRouter} from 'react-router-dom'
 import {AsyncComponentProvider} from 'react-async-component'
 import asyncBootstrapper from 'react-async-bootstrapper'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
 
 import App from 'shared/components/App'
+import reducers from 'shared/reducers'
 
+const store = createStore(reducers)
 const hook = document.getElementById('hook')
 const content = (
-  <AsyncComponentProvider rehydrateState={window.__ASYNC_COMPONENTS_STATE}>
-    <BrowserRouter>
-      <App/>
-    </BrowserRouter>
-  </AsyncComponentProvider>
+  <Provider store={store}>
+    <AsyncComponentProvider rehydrateState={window.__ASYNC_COMPONENTS_STATE}>
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
+    </AsyncComponentProvider>
+  </Provider>
 )
 
 asyncBootstrapper(content).then(() => render(content, hook))
--- source/server/reactMiddleware.js
+++ source/server/reactMiddleware.js
@@ -3,20 +3,26 @@ import {renderToString} from 'react-dom/server'
 import {StaticRouter} from 'react-router'
 import {AsyncComponentProvider, createAsyncContext} from 'react-async-component'
 import asyncBootstrapper from 'react-async-bootstrapper'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
 
 import createLayout from './createLayout'
 import App from 'shared/components/App'
+import reducers from 'shared/reducers'
 
 export default async ctx => {
   const asyncContext = createAsyncContext()
   const routerContext = {}
+  const store = createStore(reducers)
 
   const jsx = (
-    <AsyncComponentProvider asyncContext={asyncContext}>
-      <StaticRouter location={ctx.request.url} context={routerContext}>
-        <App />
-      </StaticRouter>
-    </AsyncComponentProvider>
+    <Provider store={store}>
+      <AsyncComponentProvider asyncContext={asyncContext}>
+        <StaticRouter location={ctx.request.url} context={routerContext}>
+          <App />
+        </StaticRouter>
+      </AsyncComponentProvider>
+    </Provider>
   )
   await asyncBootstrapper(jsx)
   const content = renderToString(jsx)
